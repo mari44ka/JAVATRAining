c38f28dcf6780085107bd2122c41a89a2432c7e3
mari44ka <marikostenko83@gmail.com>
2019-09-25 13:22:53 -0700

    added new function to find size of square.

--- sandbox/src/main/java/ru/stq/sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru/stq/sandbox/MyFirstProgramm.java
@@ -3,6 +3,13 @@ package ru.stq.sandbox;
 public class MyFirstProgramm {
 
     public static void main(String[] args) {
-        System.out.println("Hello,world!");
+        String somebody = "world";
+        System.out.println("Hello," + somebody+" !");
+        double a = 5;
+        System.out.println("size of square is " + area (a) + ".");
+    }
+    public static double area(double a){
+        double s = a*a;
+        return s;
     }
 }
