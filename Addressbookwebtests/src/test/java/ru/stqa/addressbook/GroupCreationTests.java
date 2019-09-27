35233e804f5ea5872e6d3cafb3a341b569c1fa1c
mari44ka <marikostenko83@gmail.com>
2019-09-27 13:06:53 -0700

    created TestBase class

--- Addressbookwebtests/src/test/java/ru/stqa/addressbook/GroupCreationTests.java
+++ Addressbookwebtests/src/test/java/ru/stqa/addressbook/GroupCreationTests.java
@@ -1,32 +1,7 @@
 package ru.stqa.addressbook;
-
-import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class GroupCreationTests {
-  private WebDriver driver;
-
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-    System.setProperty("webdriver.gecko.driver","/Users/Mari/Downloads/geckodriver");
-    driver = new FirefoxDriver();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    driver.get("http://localhost/addressbook/index.php");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys(username);
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys(password);
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
-    //driver.findElement(By.xpath("//body")).click();
-  }
+public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
@@ -39,59 +14,6 @@ public class GroupCreationTests {
 
   }
 
-  private void returnToGroupPage() {
-    driver.findElement(By.linkText("groups")).click();
-  }
 
-  private void submitForm() {
-    driver.findElement(By.name("submit")).click();
-  }
-
-  private void fillForm(GroupData groupData) {
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-  }
-
-  private void initiateGroupCreation() {
-    driver.findElement(By.name("new")).click();
-  }
-
-  private void goToGroupPage() {
-    driver.findElement(By.linkText("groups")).click();
-  }
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    driver.quit();
-
-
-  }
-
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
-
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
-
-
-  }
+}
 
---
+++ Addressbookwebtests/src/test/java/ru/stqa/addressbook/TestBase.java
@@ -0,0 +1,88 @@
+package ru.stqa.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    private WebDriver driver;
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+      System.setProperty("webdriver.gecko.driver","/Users/Mari/Downloads/geckodriver");
+      driver = new FirefoxDriver();
+      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      driver.get("http://localhost/addressbook/index.php");
+      login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+      driver.findElement(By.name("user")).click();
+      driver.findElement(By.name("user")).clear();
+      driver.findElement(By.name("user")).sendKeys(username);
+      driver.findElement(By.name("pass")).click();
+      driver.findElement(By.name("pass")).clear();
+      driver.findElement(By.name("pass")).sendKeys(password);
+      driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+      //driver.findElement(By.xpath("//body")).click();
+    }
+
+    protected void returnToGroupPage() {
+      driver.findElement(By.linkText("groups")).click();
+    }
+
+    protected void submitForm() {
+      driver.findElement(By.name("submit")).click();
+    }
+
+    protected void fillForm(GroupData groupData) {
+      driver.findElement(By.name("group_name")).click();
+      driver.findElement(By.name("group_name")).clear();
+      driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+      driver.findElement(By.name("group_header")).click();
+      driver.findElement(By.name("group_header")).clear();
+      driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+      driver.findElement(By.name("group_footer")).click();
+      driver.findElement(By.name("group_footer")).clear();
+      driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    protected void initiateGroupCreation() {
+      driver.findElement(By.name("new")).click();
+    }
+
+    protected void goToGroupPage() {
+      driver.findElement(By.linkText("groups")).click();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+      driver.quit();
+
+
+    }
+
+    private boolean isElementPresent(By by) {
+      try {
+        driver.findElement(by);
+        return true;
+      } catch (NoSuchElementException e) {
+        return false;
+      }
+    }
+
+    private boolean isAlertPresent() {
+      try {
+        driver.switchTo().alert();
+        return true;
+      } catch (NoAlertPresentException e) {
+        return false;
+      }
+    }
+}
